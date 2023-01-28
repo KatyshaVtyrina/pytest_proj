71c1b1d90b374002c679e1f8f2db4a34a8280136
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-01-28 18:30:46 +0300

    correction arrs.py and test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,19 +1,33 @@
+import pytest
+
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+@pytest.fixture
+def coll():
+    return [1, 2, 3, 4]
 
 
-def test_slice_positive_start():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_get_positive(coll):
+    assert arrs.get(coll, 1, "test") == 2
 
 
-def test_slice_negative_start():
-    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
-    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+def test_get_negative(coll):
+    assert arrs.get(coll, -1, "test") == "test"
+
+
+def test_get_zero(coll):
+    assert arrs.get(coll, 0, "test") == 1
+
+
+def test_slice_positive_start(coll):
+    assert arrs.my_slice(coll, 1, 3) == [2, 3]
+    assert arrs.my_slice(coll, 1) == [2, 3, 4]
+
+
+def test_slice_negative_start(coll):
+    assert arrs.my_slice(coll, -2) == [3, 4]
+    assert arrs.my_slice(coll, -5) == [1, 2, 3, 4]
 
 
 def test_slice_empty_list():
